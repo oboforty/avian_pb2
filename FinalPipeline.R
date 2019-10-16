494de2e3be23a3d82aa6a93970018564c68bce85
KarNair <karthikkuttan@gmail.com>
2019-10-16 19:38:27 +0200

    Tidy Up and New Results

--- FinalPipeline.R
+++ FinalPipeline.R
@@ -6,48 +6,27 @@ library(R.ROSETTA)
 library(VisuNet)
 
 
-###############################
-#                             #
-#     Input Parameters        #
-#                             #
-###############################
 
-filename <- "data/Project5.csv"
-output_dir <- "output"
-mcfs_result_file <- "temp/mcfs_run.rds"
-features_filename <- "temp/most_sig_names.rds"
+##### FUNCTION DEFINITIONS #####
 
-# MCFS parameters
-n_projections <- 10000
-proj_size <- 'auto'
-cutoff_method <- 'kmeans'
-cutoff_pe <- 20
+load_protein_IS <- function(filename) {
+  
+  #This function loads file without header and provides a work around for a bug where T and F proteins are interpreted as booleans
+  data <- read.table(filename, sep="\t", header=FALSE)
+  
+  # drop the header row and readd it as column names
+  first_row <- data[1,]
+  data <- data[-1,]
+  colnames(data) <- as.character(unlist(first_row))
+  data <- droplevels(data)
+  
+  return(data)
+}
 
-splits <- 5
-splitset_size <- 0.66
-
-# Rosetta parameters
-# StandardVoter, ObjectTrackingVoter, NaiveBayesClassifier
-classifier <- "StandardVoter"
-# Johnson, Genetic
-reducer <- "Genetic"
-
-#cvNum <- 10
-#JohnsonParam <- list(Modulo=TRUE, BRT=FALSE, BRTprec=0.9, 
-#  Precompute=FALSE, Approximate=TRUE, Fraction=0.95)
-#GeneticParam <- list(Modulo=TRUE, BRT=FALSE, BRTprec=0.9,
-#  Precompute=FALSE, Approximate=TRUE, Fraction=0.95, Algorithm="Simple")
-
-# ROC parameters
-host_clroc <- 'human'
-
-###############################
-#                             #
-#        Load datasets        #
-#                             #
-###############################
 
 load_mcfs_results <- function() {
+  
+  #This function loads saved mcfs result files from working directory
   if (exists("mcfs_result"))
     return (mcfs_result)
   
@@ -58,48 +37,52 @@ load_mcfs_results <- function() {
   }
 }
 
-load_protein_IS <- function(filename) {
-  # loads file without header
-  # this fixes a bug where T and F proteins are interpreted as booleans
-  data <- read.table(filename, sep="\t", header=FALSE)
 
-  # drop the header row and readd it as column names
-  first_row <- data[1,]
-  data <- data[-1,]
-  colnames(data) <- as.character(unlist(first_row))
-  data <- droplevels(data)
 
-  return(data)
-}
+
+
+##### FILE LOADING #####
+filename <- "data/Project5.csv"
+output_dir <- "output"
+#mcfs_result_file <- "temp/mcfs_run.rds"   #Uncomment if using saved mcfs files
+#features_filename <- "temp/most_sig_names.rds" #Uncomment if using saved features file
+
 
 data = load_protein_IS(filename)
-
 
 # Data metrics:
 dim(data)
 attributes(data)
-
 attr = attributes(data)$names
 #print(paste("Attributes per tree:", length(attr)*proj_size))
 
 # Check if the dataset is balanced
 table(data$Host)
 
-###############################
-#                             #
-#            MCFS             #
-#                             #
-###############################
 
-mcfs_result <- mcfs(Host~., data, projections=n_projections,projectionSize=proj_size, splits=splits, splitSetSize=splitset_size,
+
+
+##### MONTE CARLO FEATURE SELECTION #####
+
+#Parameters
+n_projections <- 10000
+proj_size <- 'auto'
+cutoff_method <- 'kmeans'
+cutoff_pe <- 20
+splits <- 5
+splitset_size <- 0.66
+
+
+mcfs_result <- mcfs(Host~., data, projections=n_projections,projectionSize=proj_size, splits=splits, splitSetSize=splitset_size,cutoffMethod = cutoff_method,
                cutoffPermutations = cutoff_pe, threadsNumber = 8)
+
 
 head(mcfs_result$RI)
 plot(mcfs_result, type="distances")
 
+
 # Cache the results:
 saveRDS(mcfs_result, mcfs_result_file)
-
 mcfs_result <- load_mcfs_results()
 
 
@@ -112,30 +95,39 @@ most_sig_names <- most_sig$attribute
 #most_sig_names <- readRDS(features_filename)
 
 
-###############################
-#                             #
-#          Rosetta            #
-#                             #
-###############################
+#Significant Feature Extraction
 rule_df <- select(data, most_sig_names, Host)
 
-#write.csv(rule_df,file = "sig_feat_table.csv")
+#write.csv(rule_df,file = "sig_feat_table.csv")  #Uncomment to save the file as csv
 
 
-#classifier = ObjectTrackingVoter. StandardVoter, NaiveBayesClassifier
-#reducer = Johnson, Genetic
 
 
-rules =rosetta(rule_df,roc = TRUE, discrete=TRUE, clroc = host_clroc, classifier = classifier, reducer = reducer)
+##### ROSETTA RULE BASED CLASSIFICATION #####
+classifier <- "StandardVoter" # StandardVoter, ObjectTrackingVoter, NaiveBayesClassifier
+reducer <- "Johnson" # Johnson, Genetic
 
+#JohnsonParam <- list(Modulo=TRUE, BRT=FALSE, BRTprec=0.9, Precompute=FALSE, Approximate=TRUE, Fraction=0.95)  #Uncomment if required
+#GeneticParam <- list(Modulo=TRUE, BRT=FALSE, BRTprec=0.9, Precompute=FALSE, Approximate=TRUE, Fraction=0.95, Algorithm="Simple") #Uncomment if required
+
+# Run Rosetta with ROC enabled
+rules <- rosetta(rule_df,roc = TRUE, discrete=TRUE, clroc = "Human", classifier = classifier, reducer = reducer)
+ruletable <- rules$main
+viewRules(ruletable)
+
+# Rule Quality
 rules$quality
 
+#Performance Visualisation
 rules$ROC.stats
-
-viewRules(rules$main)
-
-
 plotMeanROC(rules)
+
+
+
+##### VISUNET VISUALISATION #####
+visunet(ruletable)
+
+
 
 
 
